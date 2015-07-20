0ebeedafcf8dc77fe244343be459e2feadce23ee
Juan Manuel Tastzian <jm@tast.com.ar>
2015-07-19 23:23:43 -0300

    add migration and passing test

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Subject do
   it 'has a name' do
-    expect(Subject.new(name: 'Algebra').name).to be('Algebra')
+    expect(Subject.new(name: 'Algebra').name).to eq('Algebra')
   end
 end
